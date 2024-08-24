361402a94def743d0e72f121ec886b7722238743
VGabrielaMelo <vgmdasilva@gmail.com>
2024-08-24 11:34:12 -0300

    Listagem dos produtos.

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -11,7 +11,7 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
 
-    public void cadastrarProduto (ProdutosDTO produto){
+    public void cadastrarProduto (ProdutosDTO produto) {
 
         conn = new conectaDAO().connectDB();
 
@@ -33,6 +33,42 @@ public class ProdutosDAO {
     }
     
     public ArrayList<ProdutosDTO> listarProdutos() {
+        ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+        conectaDAO conexao = new conectaDAO();
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = conexao.connectDB();
+            String sql = "SELECT * FROM produtos";
+            pstmt = conn.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(rs.getInt("id"));
+                produto.setNome(rs.getString("nome"));
+                produto.setValor(rs.getInt("valor"));
+                produto.setStatus(rs.getString("status"));
+
+                listagem.add(produto);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        } finally {
+
+            try {
+                if (rs != null) rs.close();
+                if (pstmt != null) pstmt.close();
+                if (conn != null) conn.close();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
+        }
         
         return listagem;
     }
