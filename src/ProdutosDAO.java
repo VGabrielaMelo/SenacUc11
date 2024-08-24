7fad3b2f656d0634ed9042fcd532c6231f1d4f3d
VGabrielaMelo <vgmdasilva@gmail.com>
2024-08-24 11:29:40 -0300

    Botão de salvar produno no cadastro.

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,19 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-
 
 public class ProdutosDAO {
     
@@ -21,22 +10,30 @@ public class ProdutosDAO {
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
+
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+
+        conn = new conectaDAO().connectDB();
+
+        String sql = "INSERT INTO PRODUTOS (nome,valor,status) VALUES (?,?,?)";
+
+        try {
+            PreparedStatement prep = conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            prep.executeUpdate();
+
+            JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso");
+
+        } catch (Exception e) {
+            System.out.println("Erro ao cadastrar " + e.getMessage());
+
+        }
     }
     
-    public ArrayList<ProdutosDTO> listarProdutos(){
+    public ArrayList<ProdutosDTO> listarProdutos() {
         
         return listagem;
     }
-    
-    
-    
-        
 }
-
